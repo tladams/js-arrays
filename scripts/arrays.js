da7e7427ed79db86126807db5fda336d97c3cd00
tladams <adams3tl@dukes.jmu.edu>
2022-10-19 13:25:34 -0400

    Initial commit

---
+++ scripts/arrays.js
@@ -0,0 +1,6 @@
+console.log(data);
+// 1. instead of creating the cards manually, we should use array functions to convert the data into cards
+
+// 2. maybe we only show those that match the search query?
+
+// 3. we update the result count and related summary info as we filter
---
+++ scripts/data.js
@@ -0,0 +1,34 @@
+// with thanks to https://catalog.jmu.edu/content.php?filter%5B27%5D=CS&filter%5B29%5D=&filter%5Bcourse_type%5D=-1&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=50&expand=&navoid=2576&search_database=Filter#acalog_template_course_filter
+
+// probably helpful to have a few more in here
+const data = {
+  items: [
+    {
+      prefix: "CS",
+      number: 149,
+      title: "Introduction to Programming",
+      url: "https://catalog.jmu.edu/preview_course_nopop.php?catoid=50&coid=257219",
+      desc: "Fundamental problem-solving techniques using a modern programming language. Topics include variables, input/output, decisions, loops, functions, arrays, and objects. Students learn about algorithm development, testing strategies, and software tools.",
+      prereqs: [],
+      credits: 3,
+    },
+    {
+      prefix: "CS",
+      number: 159,
+      title: "Advanced Programming",
+      url: "https://catalog.jmu.edu/preview_course_nopop.php?catoid=50&coid=257220",
+      desc: "Students use advanced problem-solving strategies to develop algorithms using classes and objects and techniques such as recursion, exceptions and file I/O. This course also focuses on designing small applications and effective testing strategies.",
+      prereqs: [149],
+      credits: 3,
+    },
+    {
+      prefix: "CS",
+      number: 343,
+      title: "Application Development",
+      url: "https://catalog.jmu.edu/preview_course_nopop.php?catoid=50&coid=264748",
+      desc: "Overview of application software development fundamentals and their use in building stand-alone applications, visualizing and interacting with complex data representations, and controlling the Internet of Things. This course provides an introduction to the technologies used to create modern user interfaces (e.g., web technologies), establishing a common foundation for later application-focused courses.",
+      prereqs: [159],
+      credits: 3,
+    },
+  ],
+};
